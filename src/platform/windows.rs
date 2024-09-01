a8b3113a7415860759f7450832c1b06161922ad8
Tony B <krakjn@gmail.com>
2024-09-01 08:11:53 -0700

    using the bindings, windows is still broken

--- src/platform/windows.rs
+++ src/platform/windows.rs
@@ -1,14 +1,16 @@
 use std::ptr::null_mut;
-use winapi::um::handleapi::CloseHandle;
-use winapi::um::powerbase::{
-    PowerRequestDisplayRequired, PowerRequestSystemRequired, PowerSetRequest,
+use windows::Win32::Foundation::HANDLE;
+use windows::Win32::System::Power::{
+    PowerCreateRequest, PowerSetRequest, PowerRequestDisplayRequired, PowerRequestSystemRequired,
 };
-use winapi::um::winbase::PowerCreateRequest;
-use winapi::um::winnt::{POWER_REQUEST_CONTEXT_VERSION, REASON_CONTEXT};
+use windows::Win32::System::SystemServices::{CloseHandle};
+use windows::Win32::System::Threading::{
+    REASON_CONTEXT, REASON_CONTEXT_Reason, REASON_CONTEXT_Detailed, POWER_REQUEST_CONTEXT_VERSION,
+};
 
 pub fn inhibit_sleep() {
     let power_request = create_power_request();
-    if power_request.is_null() {
+    if power_request.is_invalid() {
         eprintln!("Failed to create power request.");
         std::process::exit(1);
     }
@@ -16,30 +18,38 @@ pub fn inhibit_sleep() {
     set_power_request(power_request);
 
     println!("Sleep inhibited. Press Enter to release inhibition...");
-    crate::common::wait_for_user_input();
+    crate::wait_for_user_input();
 
     release_inhibition_windows(power_request);
     println!("Inhibition released.");
 }
 
-fn create_power_request() -> *mut std::ffi::c_void {
-    let mut context = REASON_CONTEXT {
+fn create_power_request() -> HANDLE {
+    let reason = REASON_CONTEXT_Reason {
+        Detailed: REASON_CONTEXT_Detailed {
+            LocalizedReasonModule: null_mut(),
+            LocalizedReasonId: 0,
+            ReasonStringCount: 0,
+            ReasonStrings: null_mut(),
+        },
+    };
+    let context = REASON_CONTEXT {
         Version: POWER_REQUEST_CONTEXT_VERSION,
         Flags: 0,
-        Reason: null_mut(),
+        Reason: reason,
     };
 
-    unsafe { PowerCreateRequest(&mut context) }
+    unsafe { PowerCreateRequest(&context) }
 }
 
-fn set_power_request(power_request: *mut std::ffi::c_void) {
+fn set_power_request(power_request: HANDLE) {
     unsafe {
         PowerSetRequest(power_request, PowerRequestSystemRequired);
         PowerSetRequest(power_request, PowerRequestDisplayRequired);
     }
 }
 
-fn release_inhibition_windows(power_request: *mut std::ffi::c_void) {
+fn release_inhibition_windows(power_request: HANDLE) {
     unsafe {
         CloseHandle(power_request);
     }
