e2a147d891c373478df3c9730db6e52a4c7bb0d2
Tony B <krakjn@gmail.com>
2024-09-01 11:32:40 -0700

    fix shell.nix

--- shell.nix
+++ shell.nix
@@ -9,7 +9,7 @@ mkShell {
   PKG_CONFIG_PATH = "${dbus}/lib/pkgconfig";
 
   shellHook = ''
-	rustup target default
+	rustup default stable
 	#rustup target add x86_64-unknown-linux-musl
 	#rustup default stable-x86_64-unknown-linux-musl
   '';
