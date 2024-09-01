55619ada2193ed43e22d57017a4da480b77cb900
Tony B <krakjn@gmail.com>
2024-09-01 11:31:04 -0700

    Add rustup to shell.nix

--- shell.nix
+++ shell.nix
@@ -3,7 +3,15 @@ mkShell {
   buildInputs = [
     dbus
     pkg-config
+    rustup
+    #musl
   ];
   PKG_CONFIG_PATH = "${dbus}/lib/pkgconfig";
+
+  shellHook = ''
+	rustup target default
+	#rustup target add x86_64-unknown-linux-musl
+	#rustup default stable-x86_64-unknown-linux-musl
+  '';
 }
 
